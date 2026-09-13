a8d9b58aa61fe70e52a041544f1d0a799b243953
viper-f <143018173+viper-f@users.noreply.github.com>
2026-09-12 23:23:16 -0400

    Update angularLocale for Russian locale

--- src/locale_config.ts
+++ src/locale_config.ts
@@ -10,7 +10,7 @@ export const LOCALES: LocaleDefinition[] = [
     code: 'ru-RU',
     langPrefixes: ['ru'],
     translations: () => import('./locale/ru').then(m => m.TRANSLATIONS_RU),
-    angularLocale: () => import('@angular/common/locales/ru'),
+    angularLocale: () => 'ru',
   },
   {
     code: 'ch-CH',
